54f5fe06e0cd84a7235f351b9f5c1949e9ab00ef
eyeelwhy <162867010+eyeelwhy@users.noreply.github.com>
2025-10-27 08:22:31 +0500

    Add files via upload

---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc;
+using api_docker.Controllers;
+using api_docker.ModelDb;
+using dockers_api.Classes;
+
+namespace Unit_test
+{
+    [TestFixture]
+    public class UsersControllerTests
+    {
+        [Test]
+        public void Get_WhenCalled()
+        {
+            var controller = new UsersController();
+            var result = controller.Get();
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        }
+
+        [Test]
+        public void Post_Auth_WithValidData()
+        {
+            var controller = new UsersController();
+            var authUser = new UserAuth { Login = "testuser", Password = "testpass123" };
+            var result = controller.Post(authUser);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>().Or.InstanceOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void Post_Auth_WithEmpty()
+        {
+            var controller = new UsersController();
+            var authUser = new UserAuth { Login = "", Password = "" };
+            var result = controller.Post(authUser);
+            Assert.That(result, Is.InstanceOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void Register_WithValidData()
+        {
+            var controller = new UsersController();
+            Random rnd = new Random();
+            int rnd_num = rnd.Next(10000, 99999);
+            var newUser = new UserRegDTO 
+            { 
+                Login = $"newuser123{rnd_num}", 
+                Password = $"password123{rnd_num}", 
+                Name = $"Test User {rnd_num}" 
+            };
+            var result = controller.Register(newUser);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>().Or.InstanceOf<BadRequestObjectResult>().Or.InstanceOf<NotFoundResult>());
+        }
+    }
+}
